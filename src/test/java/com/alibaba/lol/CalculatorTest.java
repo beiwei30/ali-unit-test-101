21b28998abc82261740ed41fc4c4808171d6a737
beiwei.ly <beiwei.ly@alibaba-inc.com>
2018-07-31 11:27:48 +0800

    unit tes basic

---
+++ src/test/java/com/alibaba/lol/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.alibaba.lol;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void evaluatesExpression() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.evaluate("1+2+3");
+        assertEquals(6, sum);
+    }
+}
